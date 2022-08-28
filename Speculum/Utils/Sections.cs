f8c8a0f12d0afbd782a9126755c1699e9afe662e
dabbas98 <mohammad.dabbas@beetronix.com>
2022-08-28 14:18:52 +0300

    Initail Commit

---
+++ Speculum/Utils/Sections.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Speculum.Utils
+{
+    public class Sections
+    {
+        public const string Scripts = nameof(Scripts);
+        public const string Styles = nameof(Styles);
+        public const string Modals = nameof(Modals);
+    }
+}
